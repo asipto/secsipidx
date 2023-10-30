5aa943f3f3140718576ba1152b965fe5c42ff310
Daniel-Constantin Mierla <miconda@gmail.com>
2023-10-30 12:55:36 +0100

    csecsipid/secsipid.h: define of SECSIPID_VERSION set to v1.3.0

--- csecsipid/secsipid.h
+++ csecsipid/secsipid.h
@@ -10,7 +10,8 @@
  * - corresponds to version AA.BB.CC
  * - last two hexdigits are left for patch extensions
  * - example: 0x1020000 corresponds to version 1.2.0
+ * - now set to v1.3.0
  */
-#define SECSIPID_VERSION 0x1020000
+#define SECSIPID_VERSION 0x1030000
 
 #endif
